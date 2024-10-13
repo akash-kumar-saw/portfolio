e541076ef0342a85f65892e4158b9acae442cfda
Akash Kumar Saw <57334555+akash-kumar-saw@users.noreply.github.com>
2024-10-14 02:54:29 +0530

    Updated Content.js

--- src/constant/Content.js
+++ src/constant/Content.js
@@ -75,7 +75,7 @@ export const content = {
       {
         name: "Full Stack Mobile App Development",
         logo: mobileDev,
-        skill: ["Java", "Kotlin", "Android Studio", "Swift", "Xcode", "JavaScript", "React Native", "NodeJS", "ExpressJS", "Dart", "Flutter", "API's" , "Firebase", "MySQL", "SQL", "MongoDB", "PostgreSQL", "Git/GitHub", "GitLab"],
+        skill: ["Java", "Kotlin", "Android Studio", "Swing", "Swift", "Xcode", "JavaScript", "React Native", "NodeJS", "ExpressJS", "Dart", "Flutter", "API's" , "Firebase", "MySQL", "SQL", "MongoDB", "PostgreSQL", "Git/GitHub", "GitLab"],
       },
       {
         name: "Full Stack Web Development",
